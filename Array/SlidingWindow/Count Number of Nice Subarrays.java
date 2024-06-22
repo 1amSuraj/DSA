007488e1f368063289b782ff9092bbbdcdf2cfe8
Suraj Srivastava <118651437+1amSuraj@users.noreply.github.com>
2024-06-22 14:42:05 +0530

    Create Count Number of Nice Subarrays.java

---
+++ Array/SlidingWindow/Count Number of Nice Subarrays.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int n=nums.length;
+        int ans=0;
+        int cnt[]=new int[n+1];
+        int t=0;
+        cnt[0]=1;
+        for(int v:nums)
+        {
+            t+= v & 1;
+            if(t-k>=0){
+                ans+=cnt[t-k];
+            }
+            cnt[t]++;
+        }
+        return ans;
+    }
+}
