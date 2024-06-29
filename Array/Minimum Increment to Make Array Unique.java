8a468d24bd675e7be8772bbac07a141f78b88e9a
Suraj Srivastava <118651437+1amSuraj@users.noreply.github.com>
2024-06-29 17:47:44 +0530

    Create Minimum Increment to Make Array Unique.java

---
+++ Array/Minimum Increment to Make Array Unique.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minIncrementForUnique(int[] nums) {
+        Arrays.sort(nums);
+        int ans=0;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i-1]>=nums[i])
+            {
+                int increment=nums[i-1]-nums[i]+1;
+                nums[i]+=increment;
+                ans+=increment;
+            }
+        }
+        return ans;
+    }
+}
