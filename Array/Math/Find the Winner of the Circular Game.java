4e102f8e5b1ea3f43ab370d387ea766517659ac1
Suraj Srivastava <118651437+1amSuraj@users.noreply.github.com>
2024-07-08 21:33:05 +0530

    Create Find the Winner of the Circular Game.java

---
+++ Array/Math/Find the Winner of the Circular Game.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            arr.add(i);
+        }
+
+        int i = 0; // Game starts from 1st player which is sitting at index 0 in arr
+
+        while (arr.size() > 1) {
+            int idx = (i + k - 1) % arr.size();
+            arr.remove(idx);
+            i = idx;
+        }
+
+        return arr.get(0);
+    }
+}
